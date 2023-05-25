37d61b3e54f1965cf78dd5c9a07bc581f7f8580b
Ing. Jose Dumar Jimenez Ruiz <nowen21@gmail.com>
2023-05-24 20:41:20 -0500

    se corrige error

--- resources/views/layouts/adminlte/pages/ui/modals/scripts.blade.php
+++ resources/views/layouts/adminlte/pages/ui/modals/scripts.blade.php
@@ -112,7 +112,7 @@
         body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.',
         title: 'Toast Title',
         subtitle: 'Subtitle',
-        image: '{{asset("dist/img/user3-128x128.jpg')}}")}}',
+        image: '{{asset("dist/img/user3-128x128.jpg")}}',
         imageAlt: 'User Picture',
       })
     });
